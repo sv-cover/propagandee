ac82e0d6bb1411eb1dd2dc6b9c3654475dc07623
Martijn Luinstra <martijnluinstra@gmail.com>
2023-11-05 16:08:56 +0100

    Correctly convert CMYK to sRGB

--- include/CachedPoster.class.php
+++ include/CachedPoster.class.php
@@ -67,6 +67,9 @@ class CachedPoster
         else
             return false;
 
+        $imagick->setBackgroundColor('#ffffff');
+        $imagick = $imagick->mergeImageLayers(Imagick::LAYERMETHOD_FLATTEN);
+
         $cur_height = $imagick->getImageHeight();
         $cur_width = $imagick->getImageWidth();
 
@@ -96,8 +99,28 @@ class CachedPoster
             chgrp(dirname($filename), LINUX_GROUP_NAME);
         }
 
+        // error_log($imagick->getImageColorspace() == Imagick::COLORSPACE_CMYK);
+        if ($imagick->getImageColorspace() == Imagick::COLORSPACE_CMYK) {
+            $profiles = $imagick->getImageProfiles('*', false);
+
+            // we're only interested if ICC profile(s) exist 
+            $has_icc_profile = (array_search('icc', $profiles) !== false);
+
+            // if it doesnt have a CMYK ICC profile, we add one 
+            if ($has_icc_profile === false) { 
+                $icc_cmyk = file_get_contents(dirname(__FILE__).'/profiles/USWebUncoated.icc');
+                $imagick->profileImage('icc', $icc_cmyk); 
+                unset($icc_cmyk); 
+            } 
+
+            // then we add an RGB profile 
+            $icc_rgb = file_get_contents(dirname(__FILE__).'/profiles/sRGB_v4_ICC_preference.icc');
+            $imagick->profileImage('icc', $icc_rgb);
+            unset($icc_rgb);
+        }
+        $imagick->stripImage(); // this will drop down the size of the image dramatically (removes all profiles)
+
         // Write image to cache
-        $imagick->setColorspace(Imagick::COLORSPACE_SRGB);
         $imagick->setImageFormat('jpeg');
         $imagick->writeImage( $filename );
         $imagick->destroy();
